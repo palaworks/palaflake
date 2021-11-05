8796d8c21af9f53843a9c57d1dc3d5f9f6aa0776
Thaumy <Studio_ai@outlook.com>
2021-11-05 22:42:16 +0800

    init
    added cs and fs implementation

---
+++ impl/cs/palaflake/src.cs
@@ -0,0 +1,67 @@
+﻿namespace Palaflake
+{
+    using System;
+    using System.Threading;
+    using System.Diagnostics;
+
+    public class Generator
+    {
+        private DateTime start;
+
+        private ulong machineId;
+        private ulong lastTimestamp;
+
+        private byte cb; //回拨次数
+        private ushort seq; //序列号
+
+        private object syncLock = new();
+
+        //ID结构参考
+        //11111111 11111111 11111111 11111111
+        //11111111 22222222 00445555 55555555
+
+        /// <summary>
+        /// 构造palaflake生成器
+        /// </summary>
+        /// <param name="machineId">机器ID</param>
+        /// <param name="startYear">计时起始年</param>
+        public Generator(byte machineId, ushort startYear)
+        {
+            Debug.Assert(DateTime.UtcNow.Year - startYear < 34, "The startYear cannot older than 34 years");
+
+            start = new DateTime(startYear, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            this.machineId = machineId;
+        }
+
+        public ulong Next()
+        {
+            lock (syncLock)
+            {
+                var currTimestamp = (ulong) (DateTime.UtcNow - start).TotalMilliseconds;
+
+                if (currTimestamp > lastTimestamp)
+                    seq = 0;
+                else if (currTimestamp == lastTimestamp)
+                {
+                    seq++;
+                    if (seq == 4096) //一毫秒内的请求超过4096次
+                    {
+                        Thread.Sleep(1); //阻塞一毫秒
+                        currTimestamp += 1;
+                        seq = 0;
+                    }
+                }
+                else
+                {
+                    cb++;
+                    Trace.Assert(cb < 4, "Too many clock adjustments"); //超出了最大回拨次数
+                }
+
+                lastTimestamp = currTimestamp;
+
+                return currTimestamp << 24 | machineId << 16 | (ulong) cb << 12 | seq;
+            }
+        }
+    }
+}
---
+++ impl/fs/palaflake/src.fs
@@ -0,0 +1,61 @@
+﻿namespace palaflake
+
+open System
+open System.Threading
+open System.Diagnostics
+
+type private Ordering =
+    | GT
+    | EQ
+    | LT
+
+type Generator(machineId: byte, startYear: uint16) =
+    //暂不考虑State Monad
+
+    let start =
+        Debug.Assert(DateTime.UtcNow.Year - int startYear < 34, "The startYear cannot older than 34 years")
+        DateTime(int startYear, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+
+    let mutable lastTimestamp = 0UL
+
+    let mutable cb = 0uy //回拨次数
+    let mutable seq = 0us //序列号
+
+    let syncLock = Object()
+
+    let compare a b : Ordering =
+        if a > b then GT
+        else if a = b then EQ
+        else LT
+
+    //ID结构参考
+    //11111111 11111111 11111111 11111111
+    //11111111 22222222 00445555 55555555
+
+    member this.Next() =
+        lock syncLock
+        <| fun _ ->
+            let mutable currTimestamp =
+                uint64 (DateTime.UtcNow - start).TotalMilliseconds
+
+            match compare currTimestamp lastTimestamp with
+            | GT -> seq <- 0us
+            | EQ ->
+                seq <- seq + 1us
+
+                if seq = 4096us //一毫秒内的请求超过4096次
+                then
+                    Thread.Sleep 1 //阻塞一毫秒
+                    currTimestamp <- currTimestamp + 1UL
+                    seq <- 0us
+
+            | LT ->
+                cb <- cb + 1uy
+                Trace.Assert(cb < 4uy, "Too many clock adjustments") //超出了最大回拨次数
+
+            lastTimestamp <- currTimestamp
+
+            (currTimestamp <<< 24)
+            ||| (uint64 machineId <<< 16)
+            ||| (uint64 cb <<< 12)
+            ||| uint64 seq
