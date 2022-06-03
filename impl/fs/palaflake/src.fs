10486f3c9fee69ec0fb1ac98f82befb6bd952dcb
Thaumy <Studio_ai@outlook.com>
2022-06-03 11:33:14 +0800

    dependencies update

--- impl/fs/palaflake/src.fs
+++ impl/fs/palaflake/src.fs
@@ -2,19 +2,23 @@
 
 open System
 open System.Threading
-open System.Diagnostics
-open fsharper.types.Ord
+open fsharper.op.Alias
+open fsharper.typ.Ord
 
-type Generator(machineId: byte, startYear: uint16) =
+type Palaflaker(machineId: u8, startYear: u16) =
     //暂不考虑State Monad
 
     let start =
-        Trace.Assert(DateTime.UtcNow.Year >= int startYear, "The start_year cannot be set to a future time")
-        Trace.Assert(uint16 DateTime.UtcNow.Year - startYear < 34us, "The startYear cannot older than 34 years")
-        DateTime(int startYear, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+        if DateTime.UtcNow.Year < i32 startYear then
+            failwith "The startYear cannot be set to a future time"
 
-    let mutable lastTimestamp = 0UL
+        if u16 DateTime.UtcNow.Year - startYear >= 34us then
+            failwith "The startYear cannot older than 34 years"
 
+        DateTime(i32 startYear, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+
+    let mutable lastTimestamp = 0uL
+    
     let mutable cb = 0uy //回拨次数
     let mutable seq = 0us //序列号
 
@@ -23,34 +27,35 @@ type Generator(machineId: byte, startYear: uint16) =
     //33333333 33333333 33334444 44444444
 
     member self.Next() =
-        lock self
-        <| fun _ ->
+        fun _ ->
             let utcNow = DateTime.UtcNow
 
-            //当前时间早于起始时间
-            Trace.Assert(utcNow > start, "Abnormal system time")
+            if utcNow < start then //当前时间早于起始时间
+                failwith "Abnormal system time"
 
-            let mutable currTimestamp =
-                uint64 (utcNow - start).TotalMilliseconds
+            let mutable currTimestamp = u64 (utcNow - start).TotalMilliseconds
 
             match cmp currTimestamp lastTimestamp with
             | GT -> seq <- 0us
             | EQ ->
                 seq <- seq + 1us
 
-                if seq = 4096us //一毫秒内的请求超过4096次
-                then
+                if seq >= 4096us then //一毫秒内的请求超过4096次
                     Thread.Sleep 1 //阻塞一毫秒
-                    currTimestamp <- currTimestamp + 1UL
+                    currTimestamp <- currTimestamp + 1uL
                     seq <- 0us
 
             | LT ->
                 cb <- cb + 1uy
-                Trace.Assert(cb < 4uy, "Too many clock adjustments") //超出了最大回拨次数
+
+                if cb >= 4uy then //超出了最大回拨次数
+                    failwith "Too many clock adjustments"
 
             lastTimestamp <- currTimestamp
 
-            (uint64 machineId <<< 56)
-            ||| (uint64 cb <<< 52)
+            (u64 machineId <<< 56)
+            ||| (u64 cb <<< 52)
             ||| (currTimestamp <<< 12)
-            ||| uint64 seq
+            ||| u64 seq
+
+        |> lock self
